e951e8527a2ab7d248ac2f630d63b8817f0f3619
Amal Babu M <104636242+AmalBabuM@users.noreply.github.com>
2023-04-17 14:26:52 +0530

    Add files via upload

---
+++ Array 1.1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Practice2
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            int[] arr = { -7, -5, 4, 5, 6 };
+            int[] arr2 = { -7, -3, 4, 6, 10, 15 };
+            Program.SumFind(arr);
+        }
+
+        static void SumFind(int[] arr) //Time complexity => O(n^2)
+        {
+            bool found = false;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == 0)
+                    {
+                        found = true;
+                        Console.WriteLine("True: There are numbers whose sum is 0");
+                        break;
+                    }
+                }
+
+            }
+            if (!found)
+            {
+                Console.WriteLine("False: There are No such numbers whose sum is 0");
+            }
+        }
+    }
+
+}
---
+++ Array 1.2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Practice
+{
+    internal class Program
+    {
+        static bool SumFind(int[] arr) //TIme Complexity => O(n^2)
+        {
+            int n = arr.Length;
+            Array.Sort(arr);
+            for (int i = 0; i < n - 2; i++)
+            {
+                int left = i + 1;
+                int right = n - 1;
+
+                while (left < right)
+                {
+                    int sum = arr[i] + arr[left] + arr[right];
+
+                    if (sum == 0)
+                    {
+                        return true;
+                        break;
+                    }
+                    else if (sum < 0)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { -1,-2,3,4 };
+            bool value = SumFind(arr);
+            Console.WriteLine(value);
+        }
+    }
+}
